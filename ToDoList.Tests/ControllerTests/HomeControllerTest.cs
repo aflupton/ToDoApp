ddf3f9376b37c06db2f0cd82a4db78a3bea45f18
Andrew Lupton <aflupton@gmail.com>
2018-04-23 14:31:49 -0700

    add controller test file

---
+++ ToDoList.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+
+namespace ToDoList.TestClass
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+    //code goes here
+  }
+}
