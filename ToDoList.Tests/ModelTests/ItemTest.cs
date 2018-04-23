c37e6502752bc9d62421d4e7c6634f953da7ac8e
Andrew Lupton <aflupton@gmail.com>
2018-04-23 11:42:12 -0700

    add changes

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -1,23 +1,52 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
 
-namespace ToDoList.TestTools
+
+namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
       //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
+      string item = "Walk the dog.";
+      Item newItem = new Item(item);
 
       //Act
-      string result = newItem.GetDescription();
+      string result = newItem.GetItem();
 
       //Assert
-      Assert.AreEqual(description, result);
+      Assert.AreEqual(item, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string item01 = "Walk the dog.";
+      string item02 = "Wash the dishes.";
+      Item newItem1 = new Item(item01);
+      Item newItem2 = new Item(item02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+      foreach (Item thisItem in result)
+      {
+        Console.WriteLine("Output: " + thisItem.GetItem());
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+    
+    public void Dispose()
+    {
+      Item.ClearAll();
     }
   }
 }
